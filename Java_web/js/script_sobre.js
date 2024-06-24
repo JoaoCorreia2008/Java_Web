b312c0eb92373c4753112c08b1fadf0d1647cc47
uabo6969 <147398180+uabo6969@users.noreply.github.com>
2024-06-24 11:53:55 +0100

    Add files via upload

---
+++ Java_web/js/script_sobre.js
@@ -0,0 +1,6 @@
+//console.log("Acabei de ler o script.js");
+document.getElementById("headerSobreMim").innerHTML="Joao Correia";
+//document.getElementById("txt").innerHTML = "aiaiai";
+//$(document).ready(function(){
+    $(".txt").html("aiaiai");
+//});
